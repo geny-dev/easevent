5f03c14226a5be8de3fbf18c069e7fd074e1aa5f
geny-dev <genydev35@gmail.com>
2024-01-25 20:40:27 +0900

    #6: added hosts table

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Factories\UserFactory;
 use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,20 +21,21 @@ class UserSeeder extends Seeder
      */
     public function run(Generator $faker)
     {
-        $demoUser = User::create([
-            'name'              => $faker->name,
-            'email'             => 'demo@demo.com',
-            'password'          => Hash::make('demo'),
-            'email_verified_at' => now(),
-        ]);
-        $demoUser->assignRole('Admin');
+        // $demoUser = User::create([
+        //     'name'              => $faker->name,
+        //     'email'             => 'demo@demo.com',
+        //     'password'          => Hash::make('demo'),
+        //     'email_verified_at' => now(),
+        // ]);
+        // $demoUser->assignRole('Admin');
 
-        $demoUser2 = User::create([
-            'name'              => $faker->name,
-            'email'             => 'admin@demo.com',
-            'password'          => Hash::make('demo'),
-            'email_verified_at' => now(),
-        ]);
-        $demoUser2->assignRole('Sub Admin');
+        // $demoUser2 = User::create([
+        //     'name'              => $faker->name,
+        //     'email'             => 'admin@demo.com',
+        //     'password'          => Hash::make('demo'),
+        //     'email_verified_at' => now(),
+        // ]);
+        // $demoUser2->assignRole('Sub Admin');
+        UserFactory::new()->count(10)->create();
     }
 }
